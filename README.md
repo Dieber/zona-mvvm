39a1e8f29eb834472a3f300af6f2fe41b7a51da4
Dieber <a451120068@gmail.com>
2020-04-04 18:29:34 +0800

    feat(all): bone

--- README.md
+++ README.md
@@ -1,33 +1,3 @@
-# empty-project
+一步一步做一个Vue
 
-Empty project.
-
-## Building and running on localhost
-
-First install dependencies:
-
-```sh
-npm install
-```
-
-To create a production build:
-
-```sh
-npm run build-prod
-```
-
-To create a development build:
-
-```sh
-npm run build-dev
-```
-
-## Running
-
-```sh
-node dist/bundle.js
-```
-
-## Credits
-
-Made with [createapp.dev](https://createapp.dev/)
+1. 实现一个监听Watcher
--- src/index.js
+++ src/index.js
@@ -1,1 +1,65 @@
-console.log("hello world!");
+function MYVue(options) {
+  this._template = options.template
+  this._data = options.data
+  this._mounted = options.mounted
+  this._created= options.created
+  watchAllProperty(this, this._data)
+  update(this)
+
+  // this.data = options.data()
+
+
+}
+
+function update(context) {
+  this._vd = parseToVirtualDom(context.template)
+}
+
+function parseToVirtualDom(template) {
+
+}
+
+function toVirtualDom() {
+
+}
+
+let j = new MYVue({
+  data: {
+    switch: 1,
+    goo: 2
+  }
+})
+
+
+function watchAllProperty (context, vmData) {
+  let keys = Object.keys(vmData)
+  let watchObj = {}
+  for (item of keys) {
+    watchObj[item] = vmData[item]
+    Object.defineProperty(vmData, item, {
+      set(value) {
+        // console.log(`set ${item}'s value = ${value}`)
+        update(context)
+        watchObj[item] = value
+      },
+      get() {
+        console.log(`get Value: ${watchObj[item]}`)
+        return watchObj[item]
+      }
+    })
+  }
+}
+
+
+
+let i = {
+  a: 1,
+  b: 2
+}
+
+watchAllProperty(i)
+i.b = 5
+
+
+
+
